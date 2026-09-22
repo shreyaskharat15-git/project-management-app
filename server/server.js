ae28ab3c6f1cc119596d18372cbab81d9394ee56
shreyaskharat15-git <shreyaskharat15@gmail.com>
2026-09-22 18:05:29 +0530

    Prepare ProjectFlow for production

--- server/server.js
+++ server/server.js
@@ -10,12 +10,7 @@ const taskRoutes = require("./routes/tasks");
 
 const app = express();
 
-app.use(
-  cors({
-    origin: true,
-  })
-);
-
+app.use(cors());
 app.use(express.json());
 
 app.get("/api/health", (req, res) => {
@@ -34,15 +29,11 @@ const distPath = path.join(__dirname, "../dist");
 app.use(express.static(distPath));
 
 app.get("/{*splat}", (req, res) => {
-  res.sendFile(
-    path.join(distPath, "index.html")
-  );
+  res.sendFile(path.join(distPath, "index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(
-    `ProjectFlow running on port ${PORT}`
-  );
+  console.log(`ProjectFlow running on port ${PORT}`);
 });
--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 const API_URL =
-  "http://localhost:5000/api";
+  "/api";
 
 async function request(endpoint, options = {}) {
   const token = localStorage.getItem("token");
