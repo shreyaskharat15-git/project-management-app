02fab18e0ef2198ee597bea0bd781ce45cb2175e
shreyaskharat15-git <shreyaskharat15@gmail.com>
2026-09-22 19:56:38 +0530

    Fix Vite configuration and dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import react from '@ProjectFlowjs/plugin-react'
-import { defineConfig } from 'ProjectFlow'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://ProjectFlow.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+});
